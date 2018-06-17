e372f88e23e2a749be5bb578f4e3e19efb0c4df3
AndSpi <andietat@gmail.com>
2018-06-17 16:59:55 +0200

    Still not working in Atom

--- pwdgen.py
+++ pwdgen.py
@@ -1,28 +1,41 @@
 #!python3
 #
-# A training program to generate passowrds of a given lenght from alphanumeric and
-# special characters.
+# A training program to generate passowrds of a given lenght from alphanumeric and special characters.
 #
-# Complete.
+# Why is the clipboard data only available after I exit the program?
+# Why does this not run in Atom - is there no loading of moduls?
 #
-# Might be modified to directly return the results to the windows clipboard!
 # The menu could be extended to fit various common restrictions on acceptable passwords.
-# 
+#
+#
 
-import random
-import string
+from random import choices
+from string import punctuation, ascii_letters, digits
+from tkinter import Tk
 
-n = int(input("How may characters should the password consist of?\n"))
-if n == 0:
+r = Tk()
+r.withdraw()
+
+n = input("How may characters should the password consist of?\n")
+if isinstance(n,(int,float)):
+    n = int(n)
+else:
     n = 12
-p = ""
+    print("Not a valid value! Set to {0}\n".format(n))
+
 q = (input("May it contain punctuation and special characters? (y/n)\n").lower())
 if q == "y":
-    p = string.punctuation
+    p = punctuation
+else:
+    p = ""
 
-print("{1}\n{0} characters\n{1}\nYour new PWD:{1}".format(n,"\n++++++++++++++"))
+c = "".join(choices(list(ascii_letters+digits+p),k = n))
 
-print("".join(random.choices(list(string.ascii_letters+string.digits+p),k = n)))
+print("\nYour new PWD:{0}{1}{0}Has been copied to clipboard!".format("\n"+min(n,80)*"="+"\n",c))
+
+r.clipboard_clear()
+r.clipboard_append(c)
+r.update() # now it stays on the clipboard after the window is closed
+r.destroy()
 
 input("\nPress 'Return' to exit")
-    
