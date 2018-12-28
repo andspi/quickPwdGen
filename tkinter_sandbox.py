a72eb6028250d49bb0aa7023b157e27fc7f4c6c0
AndSpi <andietat@gmail.com>
2018-12-28 16:21:27 +0100

    quetlitol

--- tkinter_sandbox.py
+++ tkinter_sandbox.py
@@ -1,6 +1,6 @@
 #!python3
 #
-# A training program to generate passowrds of a given lenght from alphanumeric and special characters.
+# A training program to generate passowords of a given lenght from alphanumeric and special characters.
 #
 # Work in progress... adding tkinter for clipboard export.
 #
@@ -15,14 +15,13 @@ import tkinter as tk
 root = tk.Tk()
 root.withdraw()
 
-pwd = "quetzacoatl"
-print("Your new PWD: >/0< has been copied to clipboard".format(pwd))
+pwd = "quetlixxxoxtol"
+print("Your new Password > {0} < has been copied to clipboard".format(pwd))
 
 root.clipboard_clear()
 root.clipboard_append(pwd)
-root.update()
-CBcontent = root.clipboard_get()
-print(CBcontent)
+
+# CBcontent = root.clipboard_get()
+# print(CBcontent)
 
 input("\nPress 'Return' to exit")
-root.destroy()
